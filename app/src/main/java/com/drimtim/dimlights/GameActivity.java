0f48ec3775d92e775bb37e40248592486576d3d1
Melnikov Semen <sema.melnikov@gmail.com>
2016-11-24 00:41:21 +0300

    Add toolbar buttons in GameActivity and OptionsActivity

--- app/src/main/java/com/drimtim/dimlights/GameActivity.java
+++ app/src/main/java/com/drimtim/dimlights/GameActivity.java
@@ -1,9 +1,12 @@
 package com.drimtim.dimlights;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.drimtim.dimlights.dimlights.R;
@@ -11,23 +14,44 @@ import com.drimtim.dimlights.dimlights.R;
 
 public class GameActivity extends AppCompatActivity {
 
+    MenuItem back;
+    MenuItem settings;
+    MenuItem sound;
+    MenuItem noSound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_toolbar_arrow);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                ConfirmDialog dialog = new ConfirmDialog();
-                dialog.setTitle("Exit");
-                dialog.show(fragmentManager, "dialog");
-            }
-        });
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_game, menu);
+        back = menu.findItem(R.id.back);
+        settings = menu.findItem(R.id.settings);
+        sound = menu.findItem(R.id.sound);
+        noSound = menu.findItem(R.id.noSound);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.equals(back)) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            ConfirmDialog dialog = new ConfirmDialog();
+            dialog.setTitle("Exit");
+            dialog.show(fragmentManager, "dialog");
+        } else if (item.equals(settings)) {
+            Intent intent = new Intent(this, OptionsActivity.class);
+            startActivity(intent);
+        } else if (item.equals(sound)) {
+            startService(new Intent(this, SoundService.class));
+        } else if (item.equals(noSound)) {
+            stopService(new Intent(this, SoundService.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/drimtim/dimlights/OptionsActivity.java
+++ app/src/main/java/com/drimtim/dimlights/OptionsActivity.java
@@ -3,26 +3,37 @@ package com.drimtim.dimlights;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.drimtim.dimlights.dimlights.R;
 
 public class OptionsActivity extends AppCompatActivity {
 
+    private MenuItem back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_toolbar_arrow);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_options, menu);
+        back = menu.findItem(R.id.back);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.equals(back)) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
